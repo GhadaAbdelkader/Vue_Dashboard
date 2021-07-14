cc37819a20389002bb1b9977b57897f795572556
Ghada Abdelkader <ghada.abdo410@gmail.com>
2021-07-14 18:22:09 +0200

    Deploy

---
+++ src/router.js
@@ -0,0 +1,38 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import Dashboard from './views/Dashboard.vue'
+import Projects from './views/Projects.vue'
+import Team from './views/Team.vue'
+
+
+Vue.use(Router)
+
+export default new Router({
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes: [
+    {
+      path: '/',
+      name: 'Dashboard',
+      component: Dashboard
+    },
+    {
+      path: '/projects',
+      name: 'Projects',
+      component: Projects
+      // route level code-splitting
+      // this generates a separate chunk (about.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      //component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
+    },
+    {
+      path: '/team',
+      name: 'Team',
+      component: Team
+      // route level code-splitting
+      // this generates a separate chunk (about.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      //component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
+    }
+  ]
+})
